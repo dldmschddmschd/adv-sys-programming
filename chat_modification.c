cb67cd2bae49bf74f21f63589ec13de8deb80cde
dldmschd <lec0414@naver.com>
2016-12-17 19:57:59 +0900

    Update chat_modification.c

--- chat_modification.c
+++ chat_modification.c
@@ -151,6 +151,23 @@ leave:
     return -1;
 }
 
+int setnonblocking(int fd)
+{
+    int flags;
+
+    
+#if defined(O_NONBLOCK)
+    
+    if (-1 == (flags = fcntl(fd, F_GETFL, 0)))
+        flags = 0;
+    return fcntl(fd, F_SETFL, flags | O_NONBLOCK);
+#else
+    
+    flags = 1;
+    return ioctl(fd, FIOBIO, &flags);
+#endif
+}     
+
 int
 launch_server(void)
 {
